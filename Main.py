3cac9e72903a81e280b97d71f6e7c902e5c9ec0b
Benji <benji@cu-engr2-1-9-10.201.33.118.int.colorado.edu>
2019-04-23 12:36:00 -0600

    Main file wasn't saved

--- Main.py
+++ Main.py
@@ -10,32 +10,36 @@ def main():
     db.SetUp()
     plot = Plot.Plot('Sensor1', 'Test', 10, 10, 10, 10, 10, 10, 10, 10, 'None', cur_airTemp = None, cur_humidity = None, cur_moisture = None, cur_soilTemp = None, cur_light_full = None, cur_light_ir = None, cur_light_lux = None)
     
+    conditionalvar = threading.Condition()
+
     #create the web app as a thread
-    t = threading.Thread(target=setUpWebApp)
+    t = threading.Thread(target=setUpWebApp,args=(conditionalvar,))
     threads.append(t)
     t.start()
 
     #create a thread does data input
-    d = threading.Thread(target=setUpDataService)
+    d = threading.Thread(target=setUpDataService,args=(conditionalvar,))
     threads.append(d)
     d.start()
     
     #there is an implicit wait here
     
 
-def setUpWebApp():
+def setUpWebApp(cv):
     #unclear if a conditional variable is required
-    webapp.create(db,36636)
+    webapp.create(db,36636,cv)
 
-def setUpDataService():
+def setUpDataService(cv):
     start = time.time()
     running = 0
     while(running < 3):
         if(time.time()-start > 3):
-            plot.get_condition()
-            db.AddSensorRecord(plot.return_current())
-            start = time.time()
-            running = running+1
+            with cv:
+                plot.get_condition()
+                db.AddSensorRecord(plot.return_current())
+                start = time.time()
+                running = running+1
+                cv.notifyAll()
 
     db.close()
     
