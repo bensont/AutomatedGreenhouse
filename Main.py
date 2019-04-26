ade688f93c64c17367323842dd8e17c1a921f4da
bensont@coloradoe.edu <bensont@colorado.edu>
2019-04-25 19:33:26 -0600

    some main comments

--- Main.py
+++ Main.py
@@ -58,8 +58,10 @@ def setUpDataService(database,cv,isrun,running):
             isrun.notifyAll()
         if(local):
             if(time.time()-start > 3):
+                # take a picture every 3 cycles for demo
                 if (count%3 == 0):
                     plot.camera_facade.Take_Picture()
+                # get current condition and decide what to do with it
                 plot.get_condition()
                 plot.check_condition()
                 with cv:
@@ -80,7 +82,7 @@ def userListner(database,cv,isrun,running):
                 with isrun:
                     running = False
                     isrun.notifyAll()
-                database.close()
+                database.Close()
                 cv.notifyAll()
     
 if __name__ == "__main__":
